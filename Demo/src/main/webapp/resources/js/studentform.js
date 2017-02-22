8ec7771234a3a65b386ca6433ed7c0877a239de8
Denis Putnam <denisputnam@verizon.net>
2017-02-22 15:54:03 -0500

    Got something to work.

--- Demo/src/main/webapp/resources/js/studentform.js
+++ Demo/src/main/webapp/resources/js/studentform.js
@@ -1,35 +1,15 @@
 "use strict"; 
 angular.module('StudentApp',[]); 
-angular.module('StudentApp').service('StudentModel', [function($http){ 
+angular.module('StudentApp').service('StudentModel', ['$http', function($http){ 
 	this.students = [{name:"NONE"},{name:"NONE2"}];
 	this.getStudents = function(){
-		alert("getStudents called.");
-		$http({method: 'GET', url: 'http://localhost:8080/myapp/students'}).
-			then( function(data, status, headers, config){
-				alert("Success!!");
-				console.log( "Error: " + status );
-				this.students = data;
-			}).
-			catch( function(data, status, headers, config){
-				alert("Failed!!");
-				console.log( "Error: " + status );
-			});
-//		$http.get('http://localhost:8080/myapp/students') 
-//		.then( function (response) { 
-//			this.students = response.data; 
-//		}. bind( this), function (response) { 
-//			console.log( response.data); 
-//		});		
-//		$http.get('http://localhost:8080/myapp/students')
-//		.then(function(response){
-//			alert( "in then");
-//			alert( response.date );
-//			this.students = response.data;
-//		}.bind(this),function(response){
-//			alert( "in bind" );
-//			console.log(response.data);
-//		});
-//		alert(response.data);
+		//alert("getStudents called.");
+		$http.get('http://localhost:8080/myapp/students') 
+		.then( function (response) { 
+			this.students = response.data; 
+		}.bind( this), function (response) { 
+			console.log( response.data); 
+		});		
 	}
 }]);
 angular.module('StudentApp').controller('StudentController', ['StudentModel', function(StudentModel){ 
