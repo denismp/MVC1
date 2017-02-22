40cf5c4eb08ab816ba42febf4f035bfbf3df7ea4
Denis Putnam <denisputnam@verizon.net>
2017-02-22 15:33:12 -0500

    Trying to get it to work.

--- Demo/src/main/webapp/resources/js/studentform.js
+++ Demo/src/main/webapp/resources/js/studentform.js
@@ -1,14 +1,35 @@
 "use strict"; 
 angular.module('StudentApp',[]); 
 angular.module('StudentApp').service('StudentModel', [function($http){ 
-	this.students = [{name:"NONE"}];
+	this.students = [{name:"NONE"},{name:"NONE2"}];
 	this.getStudents = function(){
-		$http.get('http://localhost:8080/myapp/students')
-		.then(function(response){
-			this.students = response.data;
-		}.bind(this),function(response){
-			console.log(response.data);
-		});		
+		alert("getStudents called.");
+		$http({method: 'GET', url: 'http://localhost:8080/myapp/students'}).
+			then( function(data, status, headers, config){
+				alert("Success!!");
+				console.log( "Error: " + status );
+				this.students = data;
+			}).
+			catch( function(data, status, headers, config){
+				alert("Failed!!");
+				console.log( "Error: " + status );
+			});
+//		$http.get('http://localhost:8080/myapp/students') 
+//		.then( function (response) { 
+//			this.students = response.data; 
+//		}. bind( this), function (response) { 
+//			console.log( response.data); 
+//		});		
+//		$http.get('http://localhost:8080/myapp/students')
+//		.then(function(response){
+//			alert( "in then");
+//			alert( response.date );
+//			this.students = response.data;
+//		}.bind(this),function(response){
+//			alert( "in bind" );
+//			console.log(response.data);
+//		});
+//		alert(response.data);
 	}
 }]);
 angular.module('StudentApp').controller('StudentController', ['StudentModel', function(StudentModel){ 
